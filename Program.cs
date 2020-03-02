4ac387093ba7b0c9be50080dbf2adfe95667f673
kevinpenny3 <kevin.penny3@yahoo.com>
2020-03-02 10:45:56 -0600

    phase 5 done

--- Program.cs
+++ Program.cs
@@ -7,58 +7,64 @@ namespace bankHeist {
             Console.WriteLine ("Plan Your Heist");
 
             List<Dictionary<string, string>> teamMembersList = new List<Dictionary<string, string>> ();
-            Random rand = new Random ();
-            int luckValue = rand.Next (-10, 11);
-            int bankDifficulty = 100 + luckValue;
-            int teamSkill = 0;
 
-            while (true) {
-                Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+            Console.WriteLine ("How many trial runs do you wish to attempt?");
+            int trialRuns = int.Parse (Console.ReadLine ());
 
-                Console.WriteLine ("Please Enter a Member Name");
-                string memberName = Console.ReadLine ();
-                teamMember.Add ("Name", memberName);
+            for (int i = 0; i < trialRuns; i++) {
+                Random rand = new Random ();
+                int luckValue = rand.Next (-10, 11);
+                int bankDifficulty = 100 + luckValue;
+                int teamSkill = 0;
 
-                if (memberName != "") {
+                while (true) {
+                    Dictionary<string, string> teamMember = new Dictionary<string, string> ();
 
-                    Console.WriteLine ("Please Enter a Skill Level between 1 - 10");
-                    teamMember.Add ("Skill Level", Console.ReadLine ());
+                    Console.WriteLine ("Please Enter a Member Name");
+                    string memberName = Console.ReadLine ();
+                    teamMember.Add ("Name", memberName);
 
-                    Console.WriteLine ("Please Enter a Courage Level between .0 - 2.0");
-                    teamMember.Add ("Courage", Console.ReadLine ());
+                    if (memberName != "") {
 
-                    teamMembersList.Add (teamMember);
+                        Console.WriteLine ("Please Enter a Skill Level between 1 - 10");
+                        teamMember.Add ("Skill Level", Console.ReadLine ());
+
+                        Console.WriteLine ("Please Enter a Courage Level between .0 - 2.0");
+                        teamMember.Add ("Courage", Console.ReadLine ());
+
+                        teamMembersList.Add (teamMember);
+                    } else {
+                        break;
+                    }
+
+                }
+
+                // Show each team members information
+
+                foreach (Dictionary<string, string> member in teamMembersList) {
+                    // Iterate the KeyValuePairs of the teamMembers
+                    // Console.WriteLine ("Team Member:");
+                    foreach (KeyValuePair<string, string> individual in member) {
+                        if (individual.Key == "Skill Level") {
+                            teamSkill += int.Parse (individual.Value);
+                        };
+
+                        // Console.WriteLine ($"{individual.Key}: {individual.Value}");
+                        // Console.WriteLine (teamSkill);
+                    }
+                }
+
+                // Console.WriteLine (teamSkill);
+                Console.WriteLine (teamSkill);
+                Console.WriteLine (bankDifficulty);
+
+                Console.WriteLine ("Your Teams Heist");
+
+                if (teamSkill >= bankDifficulty) {
+                    Console.WriteLine ("Your Heist Was Successful");
                 } else {
-                    break;
+                    Console.WriteLine ("Your Heist Failed");
                 }
-
-            }
-
-            // Show each team members information
-
-            foreach (Dictionary<string, string> member in teamMembersList) {
-                // Iterate the KeyValuePairs of the teamMembers
-                // Console.WriteLine ("Team Member:");
-                foreach (KeyValuePair<string, string> individual in member) {
-                    if (individual.Key == "Skill Level") {
-                        teamSkill += int.Parse (individual.Value);
-                    };
-
-                    // Console.WriteLine ($"{individual.Key}: {individual.Value}");
-                    // Console.WriteLine (teamSkill);
-                }
-            }
-
-            // Console.WriteLine (teamSkill);
-            Console.WriteLine (teamSkill);
-            Console.WriteLine (bankDifficulty);
-
-            Console.WriteLine ("Your Teams Heist");
-
-            if (teamSkill >= bankDifficulty) {
-                Console.WriteLine ("Your Heist Was Successful");
-            } else {
-                Console.WriteLine ("Your Heist Failed");
             }
 
         }
